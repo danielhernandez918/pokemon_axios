1f736ec7563cd9c0967a03a006368ecf7adc7a50
Daniel Hernandez <danielhernandez918@gmail.com>
2022-03-15 14:59:20 -0700

    update

--- src/components/FetchComponent.jsx
+++ src/components/FetchComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-// import DisplayPokemon from './DisplayPokemon'
+
 
 const FetchComponent = () => {
     const [pokemon, setPokemon] = useState([])
