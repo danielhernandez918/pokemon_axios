37c5097e1f1af68a8628434f164b6935d9fd7531
Daniel Hernandez <danielhernandez918@gmail.com>
2022-03-15 14:49:28 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FetchComponent from "./components/FetchComponent";
+
+function App() {
+  return (
+    <div >
+      <FetchComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FetchComponent.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+// import DisplayPokemon from './DisplayPokemon'
+
+const FetchComponent = () => {
+    const [pokemon, setPokemon] = useState([])
+
+
+    const fetchPokemon = async() => {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=898`)
+        console.log(response.data.results)
+        setPokemon(response.data.results)
+    }
+    return (
+        <div>
+            <button onClick={fetchPokemon}>Fetch all the Pokemon!</button>
+            {pokemon.length > 0 && pokemon.map((pokemon, index)=>{
+                return (<div key={index}>{pokemon.name}</div>)
+            })}
+        </div>
+    )
+}
+
+export default FetchComponent;
