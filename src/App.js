6ee233a44c7e909e4455f950a69ab5c4ed8d77e1
Daniel Hernandez <danielhernandez918@gmail.com>
2022-03-28 18:16:10 -0700

    deleted unneeded component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import DisplayOnLoad from './components/DisplayOnLoad';
-// import FetchComponent from "./components/FetchComponent";
 
 function App() {
   return (
